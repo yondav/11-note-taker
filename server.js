d8b9bf4640721b373314bcfb2c7e57eebb27b7d5
yondav <yonidavid.abr@gmail.com>
2021-04-12 10:22:10 -0400

    server up with post, api receiving post requests. uuid npm is generating unique id

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 // dependencies
 const express = require('express');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+
+// imported modules
+const db = require('./db/db.json');
 
 // express setup
 const app = express();
+
+// middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
 // port
@@ -12,7 +20,27 @@ const PORT = process.env.PORT || 5000;
 
 // Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.json(db));
+
+// post route for api
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  db.push(newNote);
+  console.log(
+    `New note with title: "${newNote.title}", text: "${newNote.text}" and id: ${newNote.id}to db.json`
+  );
+  fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+    if (err) throw err;
+    console.log("Couldn't post new note!");
+  });
+  res.json(newNote);
+});
+
+// *** come back to delete route for api
 
 // Start server
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
